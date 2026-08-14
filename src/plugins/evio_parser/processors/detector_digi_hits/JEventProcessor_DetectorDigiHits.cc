3ff846e787b8489407ef8ba89006281af7b6e747
Raiqa Rasool <86972954+RaiqaRasool@users.noreply.github.com>
2026-08-14 10:59:32 -0400

    fix(detector-translation): read current event hits

--- src/plugins/evio_parser/processors/detector_digi_hits/JEventProcessor_DetectorDigiHits.cc
+++ src/plugins/evio_parser/processors/detector_digi_hits/JEventProcessor_DetectorDigiHits.cc
@@ -32,27 +32,21 @@ JEventProcessor_DetectorDigiHits::JEventProcessor_DetectorDigiHits() {
     SetTypeName("JEventProcessor_DetectorDigiHits");
     SetPrefix("detector_digi_hits");
     SetCallbackStyle(CallbackStyle::ExpertMode);
-    m_fadcPulses.SetOptional(true);
-    m_fadcWaveforms.SetOptional(true);
-    m_fadcPulseIntegrals.SetOptional(true);
-    m_fadcPulseTimes.SetOptional(true);
-    m_fadcPulsePeaks.SetOptional(true);
-    m_fadcScalers.SetOptional(true);
 }
 
 void JEventProcessor_DetectorDigiHits::ProcessParallel(const JEvent& event) {
     const auto table = m_translationTables->GetTable(event.GetRunNumber());
     const auto& translator_map = m_detectorTranslators();
     routeHits<FADC250PulseHit>(
-        m_fadcPulses(), *table, translator_map, event);
+        event.Get<FADC250PulseHit>("", false), *table, translator_map, event);
     routeHits<FADC250WaveformHit>(
-        m_fadcWaveforms(), *table, translator_map, event);
+        event.Get<FADC250WaveformHit>("", false), *table, translator_map, event);
     routeHits<FADC250HallBPulseIntegralHit>(
-        m_fadcPulseIntegrals(), *table, translator_map, event);
+        event.Get<FADC250HallBPulseIntegralHit>("", false), *table, translator_map, event);
     routeHits<FADC250HallBPulseTimeHit>(
-        m_fadcPulseTimes(), *table, translator_map, event);
+        event.Get<FADC250HallBPulseTimeHit>("", false), *table, translator_map, event);
     routeHits<FADC250HallBPulsePeakHit>(
-        m_fadcPulsePeaks(), *table, translator_map, event);
+        event.Get<FADC250HallBPulsePeakHit>("", false), *table, translator_map, event);
     routeHits<FADCScalerHit>(
-        m_fadcScalers(), *table, translator_map, event);
+        event.Get<FADCScalerHit>("", false), *table, translator_map, event);
 }
--- src/plugins/evio_parser/processors/detector_digi_hits/JEventProcessor_DetectorDigiHits.h
+++ src/plugins/evio_parser/processors/detector_digi_hits/JEventProcessor_DetectorDigiHits.h
@@ -18,12 +18,6 @@ public:
     void ProcessParallel(const JEvent& event) override;
 
 private:
-    Input<FADC250PulseHit> m_fadcPulses {this};
-    Input<FADC250WaveformHit> m_fadcWaveforms {this};
-    Input<FADC250HallBPulseIntegralHit> m_fadcPulseIntegrals {this};
-    Input<FADC250HallBPulseTimeHit> m_fadcPulseTimes {this};
-    Input<FADC250HallBPulsePeakHit> m_fadcPulsePeaks {this};
-    Input<FADCScalerHit> m_fadcScalers {this};
     Service<JEventService_DetectorTranslatorsMap> m_detectorTranslators {this};
     Service<JEventService_TranslationTable> m_translationTables {this};
 };
